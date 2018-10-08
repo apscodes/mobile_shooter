63e0fe4091218b0c136d08b9d8179d577d34e5b8
apscodes <alex_scicluna@yahoo.com>
2018-10-08 12:19:46 +1100

    Added in a simple enemy AI script to move it towards it's target.

---
+++ Unity Project Files/Mobile Shooter Demo/Assets/Code/AI Controller/Enemy_Manager.cs
@@ -0,0 +1,24 @@
+﻿//*! Using namespace(s)
+using UnityEngine;
+
+/// <summary>
+/// Using Awake to set the target player, use Start to grab that reference.
+/// </summary>
+public class Enemy_Manager : MonoBehaviour
+{
+    //*! Initialised with null
+    [HideInInspector]
+    public GameObject target_player = null;
+
+
+    private void Awake()
+    {
+        //*! Find the game object with the tag "Player"
+        target_player = GameObject.FindGameObjectWithTag("Player");
+
+        if (target_player == null)
+        {
+            Debug.LogError("Player GameObject could not be found, please check the tag is set to 'Player'");
+        }
+    }
+}
---
+++ Unity Project Files/Mobile Shooter Demo/Assets/Code/AI Controller/Simple_Enemy_AI.cs
@@ -0,0 +1,55 @@
+﻿//*! Using namespace(s)
+using UnityEngine;
+
+
+/// <summary>
+/// Simple Vector3.MoveTowards the target postion logic.
+/// </summary>
+public class Simple_Enemy_AI : MonoBehaviour
+{
+
+    //*! Initialised with no target
+    private GameObject target = null;
+
+    //*! Default min_distance = 1
+    [Range(1, 10)]
+    [SerializeField]
+    private int min_distance = 1;
+
+
+    //*! Default movement speed = 1
+    [Range(1, 10)]
+    public float movement_speed = 1;
+
+
+    private void Start()
+    {
+        //*! Set the target to be of the player via the Enemy_Manager
+        target = this.transform.parent.GetComponent<Enemy_Manager>().target_player;
+    }
+
+
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Simple_Move();
+        }
+    }
+
+    /// <summary>
+    /// Only call if the target is not null
+    /// </summary>
+    private void Simple_Move()
+    {
+        //*! Move towards the target if they are more than the min_distance away
+        if ((this.transform.position - target.transform.position).magnitude > min_distance)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, movement_speed * Time.deltaTime);
+        }
+
+    }
+
+
+}
