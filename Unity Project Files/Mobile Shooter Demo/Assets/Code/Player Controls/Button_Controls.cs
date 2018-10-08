94ff6ec673a99d37413016ee4738a6b4b7a6c7b5
apscodes <apscicluna@gmail.com>
2018-10-09 05:33:10 +1100

    Removed Player Movement as it made the code harder to organise, left everything in the Button Controller for the player to interact with via the in game UI.

--- Unity Project Files/Mobile Shooter Demo/Assets/Code/Player Controls/Button_Controls.cs
+++ Unity Project Files/Mobile Shooter Demo/Assets/Code/Player Controls/Button_Controls.cs
@@ -1,5 +1,8 @@
-﻿//*! Using namespace(s)
-using UnityEngine.UI;
+﻿//*!----------------------------!*//
+//*! Programmer: Alex Scicluna
+//*!----------------------------!*//
+
+//*! Using namespaces
 using UnityEngine;
 
 
@@ -9,110 +12,204 @@ using UnityEngine;
 public class Button_Controls : MonoBehaviour
 {
 
-    private Player_Movement player_movement = null;
+    //*!----------------------------!*//
+    //*!    Public Variables
+    //*!----------------------------!*//
+    #region Public Variables
+
+    public GameObject Player;
+    public float Target_Rotation { get; set; }
+
+    #endregion
+ 
+    //*!----------------------------!*//
+    //*!    Private Variables
+    //*!----------------------------!*//
+    #region Private Variables
+
     private Game_Manager game_manager = null;
 
-    private Vector2 movement_bounds_min = Vector2.zero;
-    private Vector2 movement_bounds_max = Vector2.zero;
+    private Zone room_one;
+    private Zone room_two;
+
+    private struct Zone
+    {
+        public Vector2 bounds_min;
+        public Vector2 bounds_max;
+    }
+
+    #endregion
 
 
-
+    //*!----------------------------!*//
+    //*!    Unity Functions
+    //*!----------------------------!*//
+    #region Unity Functions
+     
+    /// <summary>
+    /// Set room bounds and a reference to the game manager
+    /// </summary>
     private void Start()
     {
-        player_movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>();
+
         game_manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game_Manager>();
 
-        movement_bounds_min.x = -5;
-        movement_bounds_max.x = 5;
+        //*! Set the bounds for Room One
+        #region Room one area
 
-        movement_bounds_min.y = -8;
-        movement_bounds_max.y = 14;
+        room_one.bounds_min.x = -5;
+        room_one.bounds_max.x = 5;
+
+        room_one.bounds_min.y = -8;
+        room_one.bounds_max.y = 2;
+
+        #endregion
+               
+        //*! Set the bounds for Room Two
+        #region Room one area
+
+        room_two.bounds_min.x = -5;
+        room_two.bounds_max.x = 5;
+
+        room_two.bounds_min.y = -8;
+        room_two.bounds_max.y = 14;
+
+        #endregion
+
     }
 
-    private bool Within_Bounds(Vector3 target_position)
-    {
-
-        if ((target_position.x >= movement_bounds_min.x && target_position.x <= movement_bounds_max.x) && target_position.z >= movement_bounds_min.y && target_position.z <= movement_bounds_max.y)
-        {
-            return true;
-        }
-        else
-        {
-
-            return false;
-        }
-
-    }
+    #endregion
 
 
+
+
+    //*!----------------------------!*//
+    //*!    Custom Functions
+    //*!----------------------------!*//
+
+    //*! Public Access
+    #region Public Functions
+
+    /// <summary>
+    /// Checking if the posible next position is within bounds before moving,
+    /// if not do nothing.
+    /// </summary>
     #region Movement Button Functions
 
     public void Move_Forward()
     {
-        if (Within_Bounds(player_movement.Target_Position + new Vector3(0, 0, 1)))
-            player_movement.Target_Position = player_movement.Target_Position += new Vector3(0, 0, 1);
+        if (Within_Bounds(Player.transform.position + Player.transform.forward + Vector3.forward))
+            Player.transform.Translate(Vector3.forward);
     }
-
 
     public void Move_Backward()
     {
-
-        if (Within_Bounds(player_movement.Target_Position + new Vector3(0, 0, -1)))
-            player_movement.Target_Position = player_movement.Target_Position += new Vector3(0, 0, -1);
+        if (Within_Bounds(Player.transform.position + Player.transform.forward - Vector3.forward))
+            Player.transform.Translate(-Vector3.forward);
     }
+
 
     public void Move_Left()
     {
-
-
-        if (Within_Bounds(player_movement.Target_Position + new Vector3(-1, 0, 0)))
-            player_movement.Target_Position = player_movement.Target_Position += new Vector3(-1, 0, 0);
+        if (Within_Bounds(Player.transform.position + Player.transform.right - Vector3.right))
+            Player.transform.Translate(-Vector3.right);
     }
 
     public void Move_Right()
     {
-
-
-        if (Within_Bounds(player_movement.Target_Position + new Vector3(1, 0, 0)))
-            player_movement.Target_Position = player_movement.Target_Position += new Vector3(1, 0, 0);
+        if (Within_Bounds(Player.transform.position + Player.transform.right + Vector3.right))
+            Player.transform.Translate(Vector3.right);
     }
 
-
-
-
     #endregion
-
-
-
+    
+    
+    /// <summary>
+    /// Rotate the player left or right 15 degrees
+    /// </summary>
     #region Rotate Button Functions
-
 
     public void Rotate_Left()
     {
-        player_movement.Target_Rotation += player_movement.transform.rotation.y - 5.0f;
-
-        player_movement.Update_Rotation();
+        Player.transform.Rotate(Vector3.up, -15);
     }
 
     public void Rotate_Right()
     {
-        player_movement.Target_Rotation += player_movement.transform.rotation.y + 5.0f;
-
-        player_movement.Update_Rotation();
+        Player.transform.Rotate(Vector3.up, 15);
     }
-
-
+ 
+        
     #endregion
-
-
-
-
+       
+    /// <summary>
+    /// Player Shooting
+    /// </summary>
     #region Fire / Shoot
 
     public void Shoot()
     {
-        player_movement.Fire();
+        //*! Create a new bullet using the game managers prefab
+        GameObject bullet = Instantiate(game_manager.Bullet_Prefab, 
+                               new Vector3(Player.transform.position.x, 
+                                           Player.transform.position.y, 
+                                           Player.transform.position.z),
+                               Player.transform.rotation);
+
+        //*! Set the target position of the bullet
+        bullet.GetComponent<Bullet_Hit>().Target_Position = transform.forward * 100;
+
+        //*! Who fired the bullet - Player Shoot
+        bullet.GetComponent<Bullet_Hit>().whom_fired_it = "Player";
+
+        //*! Auto destroy in 2.5 seconds
+        Destroy(bullet, 2.5f);
     }
 
     #endregion
+
+    #endregion
+
+
+    //*! Private Access
+    #region Private Functions
+    
+    /// <summary>
+    /// Check how many enemies are left then check the room bounds.
+    /// </summary>
+    /// <param name="target_position"></param>
+    /// <returns> If the player can move in that direction. </returns>
+    private bool Within_Bounds(Vector3 target_position)
+    {
+        if (game_manager.enemy.Count > 5)
+        {
+            if ((target_position.x >= room_one.bounds_min.x && target_position.x <= room_one.bounds_max.x) 
+             && target_position.z >= room_one.bounds_min.y && target_position.z <= room_one.bounds_max.y)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+        else
+        {
+            if ((target_position.x >= room_two.bounds_min.x && target_position.x <= room_two.bounds_max.x)
+             && target_position.z >= room_two.bounds_min.y && target_position.z <= room_two.bounds_max.y)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+
+    }
+
+    #endregion
+
 }
